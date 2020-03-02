550228bd7680a6528a3dbecce3dedab06f368f66
otori23 <azeez.otori@gmail.com>
2020-03-02 14:54:39 -0800

    onInstalled, handle new install and updates differently; preserve data on update

--- js/background.js
+++ js/background.js
@@ -2,23 +2,48 @@
 
 const newURL = 'https://esports.visionsciencelabs.com';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.alarms.clearAll();
-  chrome.browserAction.setBadgeText({ text: '' });
-  chrome.storage.sync.clear(function() {
-    if (chrome.runtime.lastError) {
-      console.log('Error while clearing storage.');
-    } else {
-      console.log('cleared storage.');
-    }
-    chrome.storage.sync.set({ reminders: [] }, function() {
-      if (chrome.runtime.lastError) {
-        console.log('Error while setting initial value for reminders.');
-      } else {
-        console.log('reminders initialized to empty.');
-      }
-    });
-  });
+chrome.runtime.onInstalled.addListener(function(details) {
+  const currentVersion = chrome.runtime.getManifest().version;
+  const previousVersion = details.previousVersion;
+  const reason = details.reason;
+
+  console.log(`Previous Version: ${previousVersion}`);
+  console.log(`Current Version: ${currentVersion}`);
+
+  switch (reason) {
+    case 'install':
+      console.log('New User installed the extension.');
+      chrome.alarms.clearAll();
+      chrome.browserAction.setBadgeText({ text: '' });
+      chrome.storage.sync.clear(function() {
+        if (chrome.runtime.lastError) {
+          console.log('Error while clearing storage.');
+        } else {
+          console.log('cleared storage.');
+        }
+        chrome.storage.sync.set({ reminders: [] }, function() {
+          if (chrome.runtime.lastError) {
+            console.log('Error while setting initial value for reminders.');
+          } else {
+            console.log('reminders initialized to empty.');
+          }
+        });
+      });
+      break;
+    case 'update':
+      console.log('User has updated their extension.');
+      chrome.alarms.getAll(function(alarms) {
+        if (alarms.length > 0) {
+          chrome.browserAction.setBadgeText({ text: 'ON' });
+        }
+      });
+      break;
+    case 'chrome_update':
+    case 'shared_module_update':
+    default:
+      console.log('Other install events within the browser');
+      break;
+  }
 });
 
 chrome.alarms.onAlarm.addListener(function() {
